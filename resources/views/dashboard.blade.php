6076535f0c248921513278766da9a1b7872094dd
alves-Moises <moisesinho_ra@hotmail.com>
2024-03-04 11:35:19 -0300

    new view? dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+<div class="conteiner">
+    <div class="row">
+        <h1 class="center">Groups</h1>
+        <div class="card">
+            <div class="title">Group_1</div>
+        </div>
+    </div>
+</div>
